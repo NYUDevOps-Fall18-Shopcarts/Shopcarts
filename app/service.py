5eba71787b91a16c2b35ede59d9dd27c2ddcad02
Sankalp Verma <sankalpverma12@gmail.com>
2018-10-17 15:27:52 -0400

    Update app/service.py
    Adding URLSs

--- app/service.py
+++ app/service.py
@@ -1,3 +1,16 @@
+"""
+Shopcarts Service
+Paths:
+------
+GET /shopcarts/{id} -Returns list of products in user's shopcart from the database
+
+POST /Shopcarts- Create a Shopcart entry specific to that user_id and product_id
+
+DELETE /shopcarts/{id} - deletes a product record in the database
+"""
+
+
+
 import sys
 import logging
 from flask import Flask, jsonify, request, url_for, make_response, abort
