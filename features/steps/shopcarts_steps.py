c418fd453002e11fa09155dd282b5e836271c366
Samidha95 <samidha8056@gmail.com>
2018-11-15 14:46:31 -0500

    Adding wait_seconds

--- features/steps/shopcarts_steps.py
+++ features/steps/shopcarts_steps.py
@@ -9,6 +9,12 @@ import requests
 from behave import *
 from compare import expect, ensure
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+WAIT_SECONDS=30
+
 BASE_URL = getenv('BASE_URL', 'http://localhost:5000/')
 
 @when('I visit the "Home Page"')
@@ -59,5 +65,10 @@ def step_impl(context, element_name, text_string):
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-    element = context.driver.find_element_by_id('flash_message')
-    expect(element.text).to_contain(message)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
